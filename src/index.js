29e961cd4e7c4d919495e200ff699d2d9bf25e28
Dedenich <sergbrst@tut.by>
2020-03-02 13:14:45 +0300

    implement reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let nArr = String(n).split("");
+  let nRes = [];
+    let k = 0;
+  for (let i = nArr.length - 1; i >= 0; i--){
+    nRes[k] = nArr[i];
+    k++;
+  }
+  return parseInt(nRes.join(""));
 }
